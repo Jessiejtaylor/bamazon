b1662f7abe5cd27a9117a2ce223d71e78297d6a3
Jessie Taylor <jtaylor@jumpcrew.com>
2020-03-17 16:45:31 -0500

    added README file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,69 +19,69 @@ connection.connect(function (err) {
 })
 
 function askQuestions() {
-  connection.query("select * from products", function(error,data){
-       console.table(data)
-       inquirer.prompt([{
-        type: "input",
-        message: "What is the ID of the product you want to buy?",
-        name: "item_id"
-    },
-    {
-        type: "input",
-        message: "How many units of the product do you want to buy?",
-        name: "numUnits"
-    }
+    connection.query("select * from products", function (error, data) {
+        console.table(data)
+        inquirer.prompt([{
+            type: "input",
+            message: "What is the ID of the product you want to buy?",
+            name: "item_id"
+        },
+        {
+            type: "input",
+            message: "How many units of the product do you want to buy?",
+            name: "numUnits"
+        }
 
-    ]).then(function (userData) {
-        
-
-        console.log(userData)
-          
-  connection.query("select * from products where item_id=?",userData.item_id, function(error,data){
-           console.table(data)
-        //    console.log(data[0].stock_quantity)
-//         var array=[{name:"phil"},{name:"jessie"}]
-//         array.name
-//         array[1].name
-// //[
-    //{
-    //  item_id:1,
-    //  product_name: "Skirt",
-    //  department:"apparel",
-    //  price: 19,
-    //  stock_quantity:4999
-//     },
-//{
-    //  item_id:1,
-    //  product_name: "Skirt",
-    //  department:"apparel",
-    //  price: 19,
-    //  stock_quantity:4999
-//     }
-
-// ]
-
-          if (userData.numUnits>data[0].stock_quantity) {
-              console.log("Insufficient quantity!")
-          }
-           var newQuantity=data[0].stock_quantity - userData.numUnits
-           var statement =     connection.query("update products set stock_quantity = ? where item_id=?",[newQuantity,userData.item_id], function(error,data2){
+        ]).then(function (userData) {
 
 
-            totalPurchase = totalPurchase +  data[0].price * userData.numUnits
-            console.log("The total cost of purchase is ",totalPurchase)
+            console.log(userData)
 
-              askQuestions()
-           })  
-        //    console.log(statement.sql)
+            connection.query("select * from products where item_id=?", userData.item_id, function (error, data) {
+                console.table(data)
+                //    console.log(data[0].stock_quantity)
+                //         var array=[{name:"phil"},{name:"jessie"}]
+                //         array.name
+                //         array[1].name
+                // //[
+                //{
+                //  item_id:1,
+                //  product_name: "Skirt",
+                //  department:"apparel",
+                //  price: 19,
+                //  stock_quantity:4999
+                //     },
+                //{
+                //  item_id:1,
+                //  product_name: "Skirt",
+                //  department:"apparel",
+                //  price: 19,
+                //  stock_quantity:4999
+                //     }
 
-      } )
+                // ]
 
-    
+                if (userData.numUnits > data[0].stock_quantity) {
+                    console.log("Insufficient quantity!")
+                }
+                var newQuantity = data[0].stock_quantity - userData.numUnits
+                var statement = connection.query("update products set stock_quantity = ? where item_id=?", [newQuantity, userData.item_id], function (error, data2) {
+
+
+                    totalPurchase = totalPurchase + data[0].price * userData.numUnits
+                    console.log("The total cost of purchase is ", totalPurchase)
+
+                    askQuestions()
+                })
+                //    console.log(statement.sql)
+
+            })
+
+
+        })
+
     })
 
-  })
 
 
-   
 }
